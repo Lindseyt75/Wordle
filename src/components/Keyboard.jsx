470b8c7bed742578304fac02726fa63abc3a3d88
Timothy Lindsey <lindseyt75@yahoo.com>
2025-03-21 14:36:42 -0400

    updated keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -24,7 +24,7 @@ const Keyboard = ({ handleKeyPress, handleEnter, keyboardColors }) => {
       ))}
       <div className="keyboard-row">
         <button onClick={() => handleKeyPress("Backspace")}>←</button>
-        <button onClick={handleEnter}>Enter</button>
+        <buttonW onClick={handleEnter}>Enter</buttonW>
       </div>
     </div>
   );
